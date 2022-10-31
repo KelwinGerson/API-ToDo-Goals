6a2f303022b1aa19d0c6d78e6503b63ede0b1128
KelwinGerson <83376362+KelwinGerson@users.noreply.github.com>
2022-10-31 06:35:05 -0300

    feat: create a user +
    validation if user already exists

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
+
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  
+  const { name, username } = request.body;
+  const userAlreadyExists = users.some(
+    (user) => user.username === username
+  );
+
+  if (userAlreadyExists) {
+    return response.status(400).json({
+      error: "User already exist !"
+    })
+  }
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  });
+
+  return response.status(201).send({
+    message: "User create with sucess !"
+  })
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
